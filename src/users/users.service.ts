259d53efd33dbd54bec692936aefee54f37a54bc
Charon <charon.s.tom@gmail.com>
2025-03-14 13:45:54 +0100

    feat(api):a calendar is now automaticly created

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
+import { Calendar } from '../calendar/entities/calendar.entity'; // Importez l'entité Calendar
 
 @Injectable()
 export class UsersService {
@@ -14,6 +15,14 @@ export class UsersService {
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     const user = this.userRepository.create(createUserDto);
+
+    // 1. Créer une instance de Calendar
+    const calendar = new Calendar();
+
+    // 2. Associer le calendrier à l'utilisateur (ESSENTIEL pour cascade: true)
+    user.calendar = calendar;
+
+    // 3. Sauvegarder l'utilisateur. Grâce à cascade: true, le calendrier sera automatiquement créé et sauvegardé en même temps.
     return await this.userRepository.save(user);
   }
 
