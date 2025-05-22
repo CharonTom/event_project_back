3b6b1946f2e97b316d7be466948327486a86fe22
Charon <charon.s.tom@gmail.com>
2025-05-22 11:30:04 +0200

    implenting swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Public } from './auth/decorators/public.decorator';
 
 @Controller()
 export class AppController {
-  constructor() {}
+  constructor(private readonly appService: AppService) {}
+
+  @Public()
+  @Get()
+  getMessage() {
+    return this.appService.getMessage();
+  }
 }
