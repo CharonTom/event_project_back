7fd7c299fc5fab497add2c201ff1ad1ef50f3654
Charon <charon.s.tom@gmail.com>
2025-06-01 10:09:58 +0200

    implementing the tests in the CI

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,16 +1,20 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
-// describe('AppController', () => {
-//   let appController: AppController;
+describe('AppController', () => {
+  let appController: AppController;
 
-//   beforeEach(async () => {
-//     const app: TestingModule = await Test.createTestingModule({
-//       controllers: [AppController],
-//       providers: [AppService],
-//     }).compile();
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
 
-//     appController = app.get<AppController>(AppController);
-//   });
-// });
+    appController = app.get<AppController>(AppController);
+  });
+
+  it('should return "The API is running"', () => {
+    expect(appController.getMessage()).toBe('The API is running');
+  });
+});
